ae36779c953663e0ba6ea4545902513258d729dc
alehuo <aleksi.huotala@helsinki.fi>
2016-11-24 19:14:39 +0200

    Lisätty mapping resursseille, jotta bootstrap ja jQuery toimivat

---
+++ src/main/java/com/alehuo/wepas2016projekti/configuration/AppConfiguration.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2016 alehuo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.alehuo.wepas2016projekti.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author alehuo
+ */
+@Configuration
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+
+    /**
+     * Map resources
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
