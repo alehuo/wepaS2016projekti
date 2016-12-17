e588a0ecd9152cd0a87dfac60253afbb9efc9208
alehuo <aleksi.huotala@helsinki.fi>
2016-12-18 01:51:10 +0200

    Piilotettu tiedostoja Coverallsilta

--- src/test/java/com/alehuo/wepas2016projekti/test/CommentDomainTest.java
+++ src/test/java/com/alehuo/wepas2016projekti/test/CommentDomainTest.java
@@ -19,7 +19,6 @@ package com.alehuo.wepas2016projekti.test;
 import com.alehuo.wepas2016projekti.domain.Comment;
 import com.alehuo.wepas2016projekti.domain.UserAccount;
 import java.util.Date;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
